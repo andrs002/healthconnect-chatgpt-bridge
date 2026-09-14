365e244db22e12ed066edefd11f137df4aa7fb4c
andrs002 <andrs002zerozero@yahoo.com>
2026-09-14 22:53:39 +0800

    Apply Neon Data API v0.4

--- android/app/src/main/java/com/example/hcbridge/Api.kt
+++ android/app/src/main/java/com/example/hcbridge/Api.kt
@@ -1,13 +1,24 @@
 package com.example.hcbridge
 
+import com.google.gson.JsonObject
+import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.POST
+import retrofit2.http.Query
 
-interface BridgeApi {
-    @POST("v1/ingest")
-    suspend fun ingest(
+interface NeonAuthApi {
+    @GET("token/anonymous")
+    suspend fun anonymousToken(): JsonObject
+}
+
+interface NeonDataApi {
+    @POST("health_records")
+    suspend fun insertHealthRecords(
         @Header("Authorization") authorization: String,
-        @Body body: IngestRequest
-    )
+        @Header("Prefer") prefer: String = "resolution=ignore-duplicates,return=minimal",
+        @Query("on_conflict") onConflict: String = "user_id,record_key",
+        @Body rows: List<NeonHealthRecord>
+    ): Response<Unit>
 }
